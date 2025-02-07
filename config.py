b8e282be50f4032b419376128a661135fbe22d75
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2025-02-07 15:46:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Telegram:
     API_ID = int(os.environ.get('API_ID'))
     API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    AUTH_USER_ID = int(os.environ.get('AUTH_USER_ID'))
+    AUTH_USER_ID = list(map(int, filter(None, os.environ.get("AUTH_USER_ID", "").split())))
     
 class Database:
     REDIS_HOST = os.environ.get('REDIS_HOST') # Example: ec2.redns.redis-cloud.com, local-elephant-58690.upstash.io
