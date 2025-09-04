fb036343f36706a4dcce2ef72e441425c120a858
mxox9 <mxox7002@gmail.com>
2025-09-04 22:03:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    # Telegram API
     API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-    # Instagram (optional)
-    INSTA_USERNAME = os.getenv("INSTA_USERNAME", None)
-    INSTA_PASSWORD = os.getenv("INSTA_PASSWORD", None)
-
-    @property
-    def has_insta_creds(self):
-        return bool(self.INSTA_USERNAME and self.INSTA_PASSWORD)
+    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
