a289834b0f2d818b7bd2c83abec677d4a67e20b4
mxox9 <mxox7002@gmail.com>
2025-09-04 21:14:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,1 @@
-class Database:
-    def __init__(self):
-        self.users = {}
-
-    def set_user_credentials(self, user_id: int, username: str, password: str):
-        self.users[user_id] = {"username": username, "password": password}
-
-    def delete_user_credentials(self, user_id: int):
-        return self.users.pop(user_id, None)
-
-    def get_user_credentials(self, user_id: int):
-        creds = self.users.get(user_id)
-        if creds:
-            return creds["username"], creds["password"]
-        return None, None
-
-db = Database()
+# No database needed (Redis removed)
